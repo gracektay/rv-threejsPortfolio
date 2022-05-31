c5cfc0b255254f410672787e7eabbaeac4745041
rvcoding93 <99891357+rvcoding93@users.noreply.github.com>
2022-05-31 13:27:59 -0400

    added new geometry and animation for it as well, removed torus in favor of new shape

--- capstonePortfolio/main.js
+++ capstonePortfolio/main.js
@@ -38,11 +38,11 @@ const material = new THREE.MeshNormalMaterial({color: 0xFF47, });
 // ** Wireframe allows you to see the internals, like a house with no drywall
 const torus = new THREE.Mesh(geometry, material); // MESH puts the geometry + material together to produce your object
 
-scene.add(torus)
+// scene.add(torus)
 
 
 
-
+//lighting for scene
 const pointLight = new THREE.PointLight(0xffffff) // PointLight illuminates light in all directions like a lightbulb
 pointLight.position.set(25,25,25) // places light in the center using x,y,z axis
 
@@ -53,6 +53,8 @@ scene.add(pointLight, ambientLight) // adds the light source to the scene
 // const gridHelper = new THREE.GridHelper(200, 50); // draws 2d GRID to scene to help with perspective and placement of objects 
 // scene.add(lightHelper, gridHelper)
 
+
+//scroll reaction
 const controls = new OrbitControls(camera, renderer.domElement); // the app will listen to DOM events with the mouse, and move the camera accordingly
 
 
@@ -103,7 +105,7 @@ scene.background = spaceTexture
 //Box with photo
 const meTexture = new THREE.TextureLoader().load('me.jpeg')
 const meBox     = new THREE.Mesh (
-  new THREE.BoxGeometry(3,3,3),
+  new THREE.BoxGeometry(9,9,9),
   new THREE.MeshStandardMaterial({map: meTexture})
 );
 scene.add(meBox)
@@ -120,6 +122,29 @@ const Earth = new THREE.Mesh(
   })
 );
 scene.add(Earth)
+//=====================================================================
+
+
+
+const Fractal = new THREE.Mesh(
+  new THREE.TorusKnotGeometry( 17.939, 1.8117, 69, 7,  15, 4),
+  new THREE.MeshNormalMaterial({color: 0xFF47, } ) 
+);
+scene.add(Fractal)
+
+
+
+
+Earth.position.z = 30;
+Earth.position.setX(-10)
+
+
+function cameraScroll () {
+
+
+
+}
+
 
 
 
@@ -127,14 +152,23 @@ scene.add(Earth)
 function animate() {  //render.render(scene, camera) must be called again, it is easier to set up a recursive function with an infinite loop to complete this task
   requestAnimationFrame(animate); // continuously updating UI
   
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.005;
+  // torus.rotation.z += 0.01;
   
+  Fractal.rotation.x += 0.01;
+  Fractal.rotation.y += 0.005;
+  Fractal.rotation.z += 0.01;
+
+  Earth.rotation.y += 0.005;
+
   controls.update();
   
   renderer.render (scene, camera);
 }  // also known as a GAME LOOP in gamaing development to keep the enviroment updating with new materials
+
+
+
 
 animate() 
 
